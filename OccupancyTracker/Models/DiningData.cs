bf93a751632b272257a1312bc768b7104cb28700
DUDEbehindDUDE <jnbrooks457@gmail.com>
2024-07-27 21:42:17 -0400

    Represent diningData percents as percents

--- OccupancyTracker/Models/DiningData.cs
+++ OccupancyTracker/Models/DiningData.cs
@@ -1,11 +1,11 @@
 namespace OccupancyTracker.Models
 {
-  public record DiningData(int CurrentSoVi, int MaxSoVi, int Current704, int Max704)
-  {
-    // percent occupancy at SoVi
-    public double PercentSoVi => Math.Round((double)CurrentSoVi / MaxSoVi * 100, 2);
+	public record DiningData(int CurrentSoVi, int MaxSoVi, int Current704, int Max704)
+	{
+		// percent occupancy at SoVi
+		public double PercentSoVi => Math.Round((double)CurrentSoVi / MaxSoVi, 2);
 
-    // percent occupancy at Social 704
-    public double Percent704 => Math.Round((double)Current704 / Max704 * 100, 2);
-  };
+		// percent occupancy at Social 704
+		public double Percent704 => Math.Round((double)Current704 / Max704, 2);
+	};
 }
